40902544fba8e0e1223ac99e03000cad04885970
joran24 <joran@utexas.edu>
2016-08-29 20:16:16 -0700

    Passed 1st test and attempted 2nd test

--- src/transformYourLife.js
+++ src/transformYourLife.js
@@ -13,17 +13,38 @@ var loop = function(collection, callback){
 
 
 // 1. Build transform.
-
+var transform = function(collection,callback){
+	var results = []
+	loop(collection,function(element){
+		results.push(callback(element))
+	})
+	return results
+}
 
 // 2. allNumbersMultipliedByThree
 var numbers = [1, 2, 3, 4, 5];
 
-
+var allNumbersMultipliedByThree = transform(numbers,function(number){
+	return number * 3
+})
+console.log(allNumbersMultipliedByThree)//[3,6,9,12,15]
 // 3. bestSentenceToUpperCase
 var bestSentence = "This is the best six week course ever!";
 
+var	bestSentenceToUpperCase = transform(bestSentence,function(letter){
+	return letter.toUpperCase()
+})
+
+console.log(bestSentenceToUpperCase) //"THIS IS THE BEST SIX WEEK COURSE EVER!"
+
 // 4. collectedContents
 var person = {name: 'Jon', greatestFear: 'fearItself'};
+
+var collectedContents = transform(person, function(value,key){
+	return value
+})
+
+console.log(collectedContents)
 
 // 5. multByThree
 
